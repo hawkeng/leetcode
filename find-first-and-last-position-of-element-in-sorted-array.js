b4ac312f1c534c3161d878f5ccc7318c2bad3f50
Adonai <hawkins.dom@gmail.com>
2020-03-09 16:20:25 -0600

    Optimized algo for log N time complexity

--- find-first-and-last-position-of-element-in-sorted-array.js
+++ find-first-and-last-position-of-element-in-sorted-array.js
@@ -17,51 +17,47 @@ Output: [-1,-1]
 */
 
 /**
+ * Time: O(log n)
+ * Space: O(1)
  * @param {Number[]} arr
  * @param {Number} target
  * @returns {Number[]} indexes
  */
-function searchRange(arr, target, diff = 0) {
-  if (!arr.length) return [-1, -1];
-
-  const idx = Math.floor(arr.length / 2);
-  if (arr[idx] === target) {
-    return findIndexes(arr, target, diff);
-  } else if (arr[idx] < target) {
-    return searchRange(arr.slice(idx + 1), target, idx + 1 + diff);
-  } else {
-    return searchRange(arr.slice(0, idx), target, diff);
+function searchRange(arr, target) {
+  const leftIdx = findIndex(arr, target, 0, true);
+  if (leftIdx === arr.length || arr[leftIdx] !== target) {
+    return [-1, -1];
   }
+  if (leftIdx === arr.length - 1) return [leftIdx, leftIdx];
+
+  const rightIdx = findIndex(arr, target, leftIdx, false);
+  return [leftIdx, rightIdx - 1];
 }
 
-function findIndexes(arr, target, diff) {
-  let firstIdx = -1;
-  let lastIdx = -1;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > target) {
-      break;
-    } else if (arr[i] === target) {
-      if (firstIdx === -1) {
-        firstIdx = i + diff;
-      } else {
-        lastIdx = i + diff;
-      }
+function findIndex(arr, target, start = 0, leftSearch) {
+  let l = start;
+  let r = arr.length;
+  while (l < r) {
+    //let mid = (l + r) / 2; // It may overflow
+    let mid = Math.floor(l + (r - l) / 2);
+    if (arr[mid] > target || (leftSearch && arr[mid] === target)) {
+      r = mid;
+    } else {
+      l = mid + 1;
     }
   }
-  if (lastIdx === -1) {
-    lastIdx = firstIdx;
-  }
-  return [firstIdx, lastIdx];
+
+  return l;
 }
 
 const assert = require("assert");
 assert.deepEqual(searchRange([5, 7, 7, 7, 7, 7, 8, 8, 10], 8), [6, 7]);
-assert.deepEqual(searchRange([5,7,7,8,8,10], 8), [3,4]);
+assert.deepEqual(searchRange([5, 7, 7, 8, 8, 10], 8), [3, 4]);
 assert.deepEqual(searchRange([5, 7, 7, 8, 8, 10], 6), [-1, -1]);
-assert.deepEqual(searchRange([1,3,3,5,7,8,9,9,9,15], 9), [6,8]);
-assert.deepEqual(searchRange([100, 150, 150, 153], 150), [1,2]);
-assert.deepEqual(searchRange([1,2,3,4,5,6,10], 9), [-1,-1]);
+assert.deepEqual(searchRange([1, 3, 3, 5, 7, 8, 9, 9, 9, 15], 9), [6, 8]);
+assert.deepEqual(searchRange([100, 150, 150, 153], 150), [1, 2]);
+assert.deepEqual(searchRange([1, 2, 3, 4, 5, 6, 10], 9), [-1, -1]);
 assert.deepEqual(searchRange([1, 1, 2, 9, 10], 9), [3, 3]);
 assert.deepEqual(searchRange([1, 1, 2, 9, 9, 10], 10), [5, 5]);
 assert.deepEqual(searchRange([1], 1), [0, 0]);
-
+assert.deepEqual(searchRange([2, 2], 2), [0, 1]);
